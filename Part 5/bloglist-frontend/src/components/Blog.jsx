d35008d102c8a4fdcf3dd7a7ec6f524b405977fb
jccastillog <91742679+jccastillog@users.noreply.github.com>
2025-02-25 15:09:33 -0500

    Actualizacion

--- Part 5/bloglist-frontend/src/components/Blog.jsx
+++ Part 5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,38 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react";
+
+
+const Blog = ({ blog }) => {
+
+  const [viewDetails, setViewDetails] = useState(false)
+
+  const toggleDetails = () => {
+    setViewDetails(!viewDetails); 
+  };
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+    backgroundColor: '#f0f0f0'
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author} {" "}
+        <button onClick={toggleDetails}>{viewDetails ? "Hide" : "View"}</button>
+      </div> 
+      {viewDetails && ( 
+        <div>
+          <p>URL: {blog.url}</p>
+          <p>Likes: {blog.likes} <button>Like</button></p> 
+          <p>Author: {blog.author}</p>
+        </div>
+      )} 
+    </div>
+  );
+};
 
 export default Blog
